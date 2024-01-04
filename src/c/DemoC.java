ceff8a32db2ec5866ac5abba5da4bc2bdda92b57
Habteab <Habteab@habteab>
2024-01-03 21:03:18 -0500

    initial commit

---
+++ src/c/DemoC.java
@@ -0,0 +1,10 @@
+package c;
+
+public class DemoC {
+
+	public static void main(String[] args) {
+		System.out.println("Wednesday");
+		System.out.println(" Sprint1 is working..");
+		System.out.println("Sptint2 is working.. ");
+	}
+}
